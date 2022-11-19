1fd92c4122796ab5e0ea2003b8b7ac8f6ca90bd0
Serafion <pzakrzewski87@gmail.com>
2022-11-19 22:06:01 +0100

    MonsterCreator refactor

--- src/main/java/pl/dungeoncrwaler/Dungeon_Crawler/MonsterCreator/MonsterCreatorConfiguration.java
+++ src/main/java/pl/dungeoncrwaler/Dungeon_Crawler/MonsterCreator/MonsterCreatorConfiguration.java
@@ -1,6 +1,14 @@
 package pl.dungeoncrwaler.Dungeon_Crawler.MonsterCreator;
 
+import org.springframework.context.annotation.Bean;
+
 public class MonsterCreatorConfiguration {
+    @Bean
+    public MonsterCreatorFacade monsterCreatorFacade(){
+        return new MonsterCreatorFacade(new MonsterMapper());
+    }
+
+    //Test configuration
     public MonsterCreatorFacade monsterCreatorFacadeTest() {
         MonsterMapper monsterMapper = new MonsterMapper();
         return new MonsterCreatorFacade(monsterMapper);
